9f96b496b5efdc5ee91bfa143622d12228df5b0d
ПК <slava_zen@mail.ru>
2024-02-25 11:50:55 +0500

    Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,31 +3,24 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
-
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
-            Properties config = readProperties("rabbit.properties");
-            List<Long> store = new ArrayList<>();
+        Properties config = readProperties("rabbit.properties");
+        try (Connection connection = initConnection(config)) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap data = new JobDataMap();
-            data.put("store", store);
-            data.put("config", config);
+            data.put("connection", connection);
             JobDetail job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
@@ -41,9 +34,8 @@ public class AlertRabbit {
             scheduler.scheduleJob(job, trigger);
             Thread.sleep(10000);
             scheduler.shutdown();
-            System.out.println(store);
-        } catch (Exception se) {
-            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -51,30 +43,18 @@ public class AlertRabbit {
         public Rabbit() {
             System.out.println(hashCode());
         }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-            store.add(System.currentTimeMillis());
-            Properties config = (Properties) context.getJobDetail().getJobDataMap().get("config");
-            try {
-                Class.forName(config.getProperty("driver-class-name"));
-                Connection connection = DriverManager.getConnection(
-                        config.getProperty("url"),
-                        config.getProperty("username"),
-                        config.getProperty("password")
-                );
-                try (PreparedStatement statement =
-                             connection.prepareStatement("INSERT INTO rabbit(created_date) VALUES (?)")) {
-                    statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
-                    statement.executeUpdate();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
+            Connection con = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement statement =
+                         con.prepareStatement("INSERT INTO rabbit(created_date) VALUES (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                statement.executeUpdate();
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
-
         }
     }
 
@@ -82,9 +62,19 @@ public class AlertRabbit {
         Properties config = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(properties)) {
             config.load(in);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return config;
+    }
+
+    private static Connection initConnection(Properties config) throws SQLException, ClassNotFoundException {
+        Class.forName(config.getProperty("driver-class-name"));
+        Connection connection = DriverManager.getConnection(
+                config.getProperty("url"),
+                config.getProperty("username"),
+                config.getProperty("password")
+        );
+        return connection;
     }
 }
